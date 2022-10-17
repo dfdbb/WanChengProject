86fe8f05c304ee60e96fa531ccaddd39c0d93c8d
dfdbb <1918959949@QQ.COM>
2022-10-17 19:25:28 +0800

    dev change to can running

--- wcdemo/src/main/java/com/example/wanchengdemo/controller/TestController.java
+++ wcdemo/src/main/java/com/example/wanchengdemo/controller/TestController.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.wanchengdemo.commom.R;
 import com.example.wanchengdemo.entity.User;
 import com.example.wanchengdemo.service.UserService;
+import com.example.wanchengdemo.util.TokenUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
