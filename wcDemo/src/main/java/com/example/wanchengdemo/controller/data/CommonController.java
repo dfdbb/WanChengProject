b5b7ac0769e33ff6a958bb9025cada654a340ba4
young <1918959949@qq.com>
2022-11-04 19:21:44 +0800

    新增对section的数据清洗

--- wcDemo/src/main/java/com/example/wanchengdemo/controller/data/CommonController.java
+++ wcDemo/src/main/java/com/example/wanchengdemo/controller/data/CommonController.java
@@ -1,7 +1,16 @@
 package com.example.wanchengdemo.controller.data;
 
 import com.example.wanchengdemo.commom.R;
+import com.example.wanchengdemo.entity.Project;
+import com.example.wanchengdemo.entity.Section;
+import com.example.wanchengdemo.entity.Segment;
+import com.example.wanchengdemo.entity.Site;
+import com.example.wanchengdemo.service.dataService.SectionService;
+import com.example.wanchengdemo.service.dataService.SegmentService;
+import com.example.wanchengdemo.service.dataService.SiteService;
+import com.example.wanchengdemo.util.FileRead;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,6 +23,16 @@ import java.nio.file.Files;
 @RequestMapping("/common")
 @Slf4j
 public class CommonController {
+
+    @Autowired
+    SectionService sectionService;
+
+    @Autowired
+    SegmentService segmentService;
+
+    @Autowired
+    SiteService siteService;
+
 
     /*
     * 文件上传
@@ -30,23 +49,37 @@ public class CommonController {
 
     @PostMapping("/upload")
     @ResponseBody
-    public String create(@RequestPart MultipartFile file) throws IOException {
+    public String create(@RequestPart MultipartFile file, String pid, Section section, Segment segment, Site site) throws IOException {
+        //将文件保存至本地
         String fileName = file.getOriginalFilename();
         String filePath = path + fileName;
 
         File dest = new File(filePath);
         Files.copy(file.getInputStream(), dest.toPath());
+
+        //读取本地文件
+        String sourceData = FileRead.txt2String(dest);
+
+        //清洗数据
+        FileRead.dataClean(sourceData);
+
         return "Upload file success : " + dest.getAbsolutePath();
     }
+
 
 
     /*
     * 文件导入数据库
     * */
+/*    @PostMapping("/up2Mysql")
+    public R<String> fileUp2Mysql(String pid, Section section, Segment segment, Site site){
 
-    @PostMapping("/")
-    public R<String> fileInSql(){
+        String filePaht = "";
+        File file = new File(filePaht);
+        String txt2String = FileRead.txt2String(file);
+
+
 
         return null;
-    }
+    }*/
 }
--- wcDemo/src/main/java/com/example/wanchengdemo/util/FileRead.java
+++ wcDemo/src/main/java/com/example/wanchengdemo/util/FileRead.java
@@ -1,7 +1,10 @@
 package com.example.wanchengdemo.util;
 
 
+import com.example.wanchengdemo.entity.Section;
+import com.example.wanchengdemo.entity.Segment;
 import com.example.wanchengdemo.entity.Site;
+import org.springframework.stereotype.Component;
 
 import java.io.*;
 
@@ -10,8 +13,8 @@ import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
-public class Txttest {
+@Component
+public class FileRead {
     /**
      * 读取txt文件的内容
      * @param file 想要读取的文件对象
@@ -41,7 +44,7 @@ public class Txttest {
     public static String txt2String(File file){
         StringBuilder result = new StringBuilder();
         try{
-            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file),"UTF-8"));//构造一个BufferedReader类来读取文件
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file),"GBK"));//构造一个BufferedReader类来读取文件
             String s = null;
             while((s = br.readLine())!=null){//使用readLine方法，一次读一行
                 result.append(System.lineSeparator()+s);
@@ -53,6 +56,63 @@ public class Txttest {
         return result.toString();
     }
 
+    public static String dataClean(String sourceData){
+
+        Section section = new Section();
+        Segment segment = new Segment();
+        Site    site    = new Site();
+        //正则表达式
+
+        //获得检测合同
+        String regSection = "文件名.*弯沉测试数据.(.*).txt";
+        //获得施工单位
+        String regScons   ="施工单位：(.*)";
+        //获得检测单位
+        String regTesting= "检测人员：(.*)";
+
+        //获得检测日期
+        String regDate= "检测日期：(.*)";
+        //获得检测段落
+        String regSegment= "检测段落：([a-zA-Z0-9+-]*)";
+        //获得路幅
+        String regRoadWay = "路幅：([左右]幅)";
+        //获得桩号处理
+        String regRoadHandle = "桩号处理：([顺逆]桩号)";
+        //获得交工验收弯沉值
+        String regDesign= "交工验收弯沉值（0.01mm）：([0-9]*)";
+        //获得路面温度
+        String regPavement_tp = "路面温度（°C）：([0-9]*)";
+        //获得弯沉数据
+        String regData= "([A-Z][0-9]{4}[+][0-9]{3}\t[m]\t[0-9]*\t[0-9]*\t[0-9]*\t[0-9]*\t[0-9]*\t[0-9]*)";
+
+        //section清洗
+        String[] sectionReg = {regSection,regScons,regTesting};
+        for (String reg : sectionReg) {
+            //计时器
+            int i = 1;
+            Pattern pattern = Pattern.compile (reg);
+            Matcher matcher = pattern.matcher (sourceData);
+
+            while (matcher.find()){
+                System.out.println(matcher.group(1));
+                //i = 1时，将 合同段 填入section对象， i = 2时，填入 施工单位 ,i = 3时，填入 检测单位
+                if (i==1){
+                    section.setSname(matcher.group(1));
+                    
+                }
+                if(i==2){
+                    section.setScons(matcher.group(1));
+                }
+                if (i == 3){
+                    section.setStesting(matcher.group(1));
+
+                }
+                ++i;
+            }
+        }
+        System.out.println();
+        return null;
+    }
     public static void main(Site site){
         File file = new File("D:\\Project\\source\\赫六5标2.txt");
 
